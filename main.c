b5f6167d700a15e31c98dcafc66dd2d15a1d06c5
PATNAM DHANUSH <patnamdhanush@DHANU-MacBook-Air.local>
2024-10-30 19:14:06 +0530

    GPIO Init

--- main.c
+++ main.c
@@ -11,4 +11,10 @@ void GPIO_PORT_F_init(void) {
     GPIO_PORTF_DIR_R = 0x0E;                   /* Set PF1, PF2, PF3 as outputs; PF0, PF4 as inputs */
     GPIO_PORTF_PUR_R = 0x11;                   /* Pull-up resistors for PF0 and PF4 */
 
+
+    NVIC_EN0_R |= 1 << 30;                     /* Enable interrupts for Port F */
+    GPIO_PORTF_IS_R = 0x00;                    /* Set edge-sensitive interrupts */
+    GPIO_PORTF_IBE_R = 0x00;                   /* Single edge triggering */
+    GPIO_PORTF_IEV_R = 0x00;                   /* Trigger on falling edge */
+    GPIO_PORTF_IM_R |= 0x11;                   /* Unmask interrupts on PF0 and PF4 */
 }
